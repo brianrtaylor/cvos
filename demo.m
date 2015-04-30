9190b26e154568e1e15e7f521e98c3ef8121d52c
Brian Tay <btay@zapp.(none)>
2015-04-30 13:36:14 -0700

    added demo file (still needs work) to run on cars5. We should probably choose another example and/or include more

---
+++ demo.m
@@ -0,0 +1,27 @@
+% TODO: use better example than cars5 because we lose the right car
+% TODO: make tao_visual save less stuff
+% in frame 1
+
+% 1. download data somewhere local
+pth = 'example_demo/';
+if ~exist(pth, 'dir'); mkdir(pth); end;
+cd(pth);
+if ~exist('moseg', 'dir') && ~exist('cars5_demo.zip', 'file');
+  unix('wget vision.ucla.edu/~btay/proj/cvos/demo/cars5_demo.zip'); % 204 MB
+end
+if ~exist('moseg', 'dir');
+  unix('unzip cars5_demo.zip');
+  % unix('rm cars5_demo.zip');
+end
+cd('..');
+
+% 2. some setup
+% mex_clean
+mex_setup
+
+% 3. run cvos_run
+% paths to results are editable in cdo_aid/dataPaths.m
+cvos_run({'cars5'}, 'full', 'moseg');
+
+% 4. see results in "./results" folder
+% TODO: visualize results here
