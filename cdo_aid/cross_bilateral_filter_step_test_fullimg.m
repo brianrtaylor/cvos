3246a8ca0a32c6cd5e46dcc2de7c1d612ecef66f
Brian Tay <btay@zapp.(none)>
2015-04-30 11:29:54 -0700

    cleaning out extraneous cbf, cross_bilateral_filter codes

--- cdo_aid/cross_bilateral_filter_step_test_fullimg.m
+++ cdo_aid/cross_bilateral_filter_step_test_fullimg.m
@@ -1,15 +1,16 @@
-%---------------------------------------------------------------------------
+%---------------------------------------------------------------------
 % how this function is called
 %
-% [uvb_cbf, uvf_cbf, occb_cbf, occf_cbf, occb_cbf_prob, occf_cbf_prob] = ...
-%   cross_bilateral_filter_step(occb, occf, uvb, uvf, uvb_rev, uvf_rev, I0, I1, I2, m);
+% [uvb_cbf, uvf_cbf, occb_cbf, occf_cbf, occb_cbf_prob, ...
+%   occf_cbf_prob] = cross_bilateral_filter_step_test_fullimg( ...
+%   occb, occf, uvb, uvf, uvb_rev, uvf_rev, I0, I1, I2, m, VIS)
 % 
-%---------------------------------------------------------------------------
-function [uvb_cbf, uvf_cbf, occb_cbf, occf_cbf, occb_cbf_prob, occf_cbf_prob] = cross_bilateral_filter_step_test_fullimg( ...
+%---------------------------------------------------------------------
+function [uvb_cbf, uvf_cbf, occb_cbf, occf_cbf, occb_cbf_prob, ...
+  occf_cbf_prob] = cross_bilateral_filter_step_test_fullimg( ...
   occb, occf, uvb, uvf, uvb_rev, uvf_rev, I0, I1, I2, m, VIS)
 
 if ~exist('VIS', 'var'); VIS = 100; end;
-
 [rows,cols,~] = size(I1);
 
 % segtrack (g4v)
@@ -72,8 +73,6 @@ else
   atic = tic();
   % b_change = ones_img;
   b_change = occb_prob;
-  % uvb_cbf = cbf_flow(uvb_safe, uvf_safe, b_change, ...
-  %   occb_prob, occf_prob, W, sigma_r_b, sigma_r_f, 2*uvb_avg);
   uvb_cbf = cbf_flow(uvb_safe, uvf_safe, b_change, ...
     occb_prob, occf_prob, W, sigma_r_b, sigma_r_f, 2*uvb_avg, exp_fxn);
   atic = toc(atic); fprintf('uvf cbf (%0.3f s)\n', atic);
@@ -86,9 +85,6 @@ else
   atic = toc(atic); fprintf('uvb cbf (%0.3f s)\n', atic);
 end
 
-% fig(3); imagex([[uvb; uvb_cbf_ay; uvb_cbf], [uvf; uvf_cbf_ay; uvf_cbf]]);
-% keyboard;
-
 uvb_cbf_safe = uvb_cbf; uvb_cbf_safe(isinf(uvb_cbf) | isnan(uvb_cbf)) = 0;
 uvf_cbf_safe = uvf_cbf; uvf_cbf_safe(isinf(uvf_cbf) | isnan(uvf_cbf)) = 0;
 
@@ -97,36 +93,16 @@ uvf_cbf_safe = uvf_cbf; uvf_cbf_safe(isinf(uvf_cbf) | isnan(uvf_cbf)) = 0;
 %-------------------------------------------------------------------
 occb_cbf = occ_calc(I1, I0, uvb_cbf, OCCMETHOD);
 occf_cbf = occ_calc(I1, I2, uvf_cbf, OCCMETHOD);
-%   occb_cbf_prob = occ_res_to_prob(occb_cbf, true);
-%   occf_cbf_prob = occ_res_to_prob(occf_cbf, true);
 
 occb_cbf_sole = cbf_occ(max(0.0, occb_cbf - occf_cbf), ...
   uvb_cbf_safe, uvf_safe, uvb_rev_safe, i1, ...
   max(uvb_avg * 2.0, 3.0), uvb_avg * 2.0, sigma_r_b, 25, 0.25);
-% note: uvf_safe instead of uvf_cbf_safe - is this good?
 occf_cbf_sole = cbf_occ(max(0.0, occf_cbf - occb_cbf), ...
   uvf_cbf_safe, uvb_safe, uvf_rev_safe, i1, ...
   max(uvf_avg * 2.0, 3.0), uvf_avg * 2.0, sigma_r_f, 25, 0.25);
-% note: uvb_safe instead of uvb_cbf_safe - is this good?
 
 occb_cbf_prob_v0 = occ_res_to_prob(occb_cbf_sole);
 occf_cbf_prob_v0 = occ_res_to_prob(occf_cbf_sole);
-
-%   occb_cbf_prob_ = occb_cbf_prob_v0 .* ( 1 - occf_cbf_prob_v0 );
-%   occf_cbf_prob_ = occf_cbf_prob_v0 .* ( 1 - occb_cbf_prob_v0 );
-
-%-------------------------------------------------------------------
-% visualizations
-%-------------------------------------------------------------------
-if VIS < 250;
-  fig(241); imagex([[uvb; uvb_cbf], [uvf; uvf_cbf]]);
-  title('uvb / uvb-cbf | uvf / uvf-cbf');
-  %     fig(242); imagesc([[occb_cbf_prob; occb_cbf_prob_v0; occb_cbf_prob_], ...
-  %       [occf_cbf_prob; occf_cbf_prob_v0; occf_cbf_prob_]]);
-  %     title(['occb-cbf-prob / occb-cbf-prob-v0 / occb-cbf-prob-' ...
-  %       '| occf-cbf-prob / occf-cbf-prob-v0 / occf-cbf-prob-']);
-  colorbar; drawnow;
-end
 
 %-------------------------------------------------------------------
 % sanitize output
